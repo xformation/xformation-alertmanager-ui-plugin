b20991e4116ee9662f1284b26415b0f2f512c57a
Jasmin Makasana <jasminmakasana1991@gmail.com>
2020-06-19 13:01:10 +0530

    Update: optimization done.

--- src/domain/AlertRuleBuilder/Message.tsx
+++ src/domain/AlertRuleBuilder/Message.tsx
@@ -1,31 +1,44 @@
 import * as React from 'react';
 
 export class Message extends React.Component<any, any>{
-
+    templateList: any;
     constructor(props: any) {
         super(props);
         this.state = {
             messageData: '',
         };
+        this.templateList = ['{{.ID}}', '{{.Name}}', '{{.TaskName}}', '{{.Group}}', '{{.Tags}}', '{{index .Tags "value"}}', '{{.Level}}', '{{.Fields}}', '{{index .Fields "value"}}', '{{.Time}}', '{{else}}', '{{end}}'];
     }
 
     addMessages = (val: any) => {
-        const { messageData } = this.state;
-        let retData = '';
-        retData = messageData;
-        retData+= '{{'+val+'}}'+' '+' ';
+        let { messageData } = this.state;
+        messageData += '  ' + val;
         this.setState({
-            messageData: retData
+            messageData
         });
     };
 
     combineMessages = (event: any) => {
-        console.log(event.target.value);
-        const { messageData } = this.state;
         this.setState({
             messageData: event.target.value
         });
     }
+
+    createTemplateList = (templateList: any) => {
+        const length = templateList.length;
+        const retData = [];
+        for (let i = 0; i < length; i++) {
+            const template = templateList[i];
+            retData.push(
+                <li onClick={() => { this.addMessages(template) }}>
+                    <pre>
+                        <code>{template}</code>
+                    </pre>
+                </li>
+            );
+        }
+        return retData;
+    };
 
     render() {
         const { messageData } = this.state;
@@ -39,79 +52,14 @@ export class Message extends React.Component<any, any>{
                         </div>
                         <div className="condition-message-box">
                             <div className="condition-message-examplex">
-                                <textarea placeholder="Example: &#0123;&#0123;.ID&#0125;&#0125; 
-                                is &#0123;&#0123;.Level&#0125;&#0125; value: &#0123;&#0123;
-                                index.Fields 'value'&#0125;&#0125;" onChange={e => { this.combineMessages(e) }}
-                                 value={messageData}>
-
-                                </textarea>
-
+                                <textarea placeholder='Example: {{ .ID }} is {{ .Level }} value: {{ index .Fields "value" }}' onChange={e => { this.combineMessages(e) }} value={messageData}></textarea>
                             </div>
                             <div className="templates-text">
                                 <ul>
                                     <li>
                                         <span>Templates:</span>
                                     </li>
-                                    <li onClick={() => { this.addMessages('.ID') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.ID&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
-                                    <li onClick={() => { this.addMessages('.Name') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.Name&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
-                                    <li onClick={() => { this.addMessages('.TaskName') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.TaskName&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
-                                    <li onClick={() => { this.addMessages('.Group') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.Group&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
-                                    <li onClick={() => { this.addMessages('.Tags') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.Tags&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
-                                    <li onClick={() => { this.addMessages('.index.Tags "value"') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.index.Tags "value"&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
-                                    <li onClick={() => { this.addMessages('.Level') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.Level&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
-                                    <li onClick={() => { this.addMessages('.Fields') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.Fields&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
-                                    <li onClick={() => { this.addMessages('.index.Fields "value"') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.index.Fields "value"&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
-                                    <li onClick={() => { this.addMessages('.Time') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.Time&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
-                                    <li onClick={() => { this.addMessages('.else') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.else&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
-                                    <li onClick={() => { this.addMessages('.end') }}>
-                                        <pre>
-                                            <code>&#0123;&#0123;.end&#0125;&#0125;</code>
-                                        </pre>
-                                    </li>
+                                    {this.createTemplateList(this.templateList)}
                                 </ul>
                             </div>
                         </div>
