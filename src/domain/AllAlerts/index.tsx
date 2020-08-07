aab897a3917556250e287575ebf70667ea9429d2
dipti96 <diptippatel96@gmail.com>
2020-08-07 14:02:48 +0530

    changes in timeRange

--- src/domain/AllAlerts/index.tsx
+++ src/domain/AllAlerts/index.tsx
@@ -6,7 +6,6 @@ import { config } from '../../config';
 import { PopupContent } from './PopupContent';
 import { severityDS } from '../_utilities/commonDS';
 import { RestService } from '../_service/RestService';
-import { TimeRangePopup } from './TimeRangePopup';
 
 export class AllAlerts extends React.Component<any, any> {
     breadCrumbs: any;
@@ -27,7 +26,28 @@ export class AllAlerts extends React.Component<any, any> {
             monitorService: "",
             alertType: "",
             severity: "",
-            alertState: ""
+            alertState: "",
+            currentTime: 'Last 6 hours',
+            fromTime: 'now-6h',
+            toTime: 'now',
+            TimeOption: [
+                { from: 'now-5m', to: 'now', display: 'Last 5 minutes', section: 3 },
+                { from: 'now-15m', to: 'now', display: 'Last 15 minutes', section: 3 },
+                { from: 'now-30m', to: 'now', display: 'Last 30 minutes', section: 3 },
+                { from: 'now-1h', to: 'now', display: 'Last 1 hour', section: 3 },
+                { from: 'now-3h', to: 'now', display: 'Last 3 hours', section: 3 },
+                { from: 'now-6h', to: 'now', display: 'Last 6 hours', section: 3 },
+                { from: 'now-12h', to: 'now', display: 'Last 12 hours', section: 3 },
+                { from: 'now-24h', to: 'now', display: 'Last 24 hours', section: 3 },
+                { from: 'now-2d', to: 'now', display: 'Last 2 days', section: 3 },
+                { from: 'now-7d', to: 'now', display: 'Last 7 days', section: 3 },
+                { from: 'now-30d', to: 'now', display: 'Last 30 days', section: 3 },
+                { from: 'now-90d', to: 'now', display: 'Last 90 days', section: 3 },
+                { from: 'now-6M', to: 'now', display: 'Last 6 months', section: 3 },
+                { from: 'now-1y', to: 'now', display: 'Last 1 year', section: 3 },
+                { from: 'now-2y', to: 'now', display: 'Last 2 years', section: 3 },
+                { from: 'now-5y', to: 'now', display: 'Last 5 years', section: 3 },
+            ],
         };
         this.breadCrumbs = [
             {
@@ -122,7 +142,6 @@ export class AllAlerts extends React.Component<any, any> {
             label: "Closed",
             value: "Closed"
         }];
-        this.timeRangeRef = React.createRef();
     }
 
     componentDidMount() {
@@ -147,7 +166,10 @@ export class AllAlerts extends React.Component<any, any> {
     }
 
     onClickopenTimeRangePopup = (e: any) => {
-        this.timeRangeRef.current.toggle();
+        let status = !this.state.openTimeRange;
+        this.setState({
+            openTimeRange: status,
+        })
     };
 
     createAllAlertsTable = () => {
@@ -283,8 +305,33 @@ export class AllAlerts extends React.Component<any, any> {
         }
     };
 
+    displayTimeRange = () => {
+        const retuData = [];
+        for (let i = 0; i < this.state.TimeOption.length; i++) {
+            let data = this.state.TimeOption[i];
+            retuData.push(
+                <option value={data.display}>{data.display}</option>
+            );
+        }
+        return retuData;
+    }
+
+    setTimeValue = (e: any) => {
+        console.log("bchdbhs");
+        console.log(e.target.value);
+        for (let i = 0; i < this.state.TimeOption.length; i++) {
+            const timeData = this.state.TimeOption[i];
+            if (timeData.display == e.target.value) {
+                this.setState({
+                    currentTime: timeData.display,
+                    fromTime: timeData.from,
+                    toTime: timeData.to,
+                })
+            }
+        }
+    }
     render() {
-        const state = this.state;
+        const { resourceGroup, resource, openTimeRange, monitorService, alertType, severity, currentTime, alertState, fromTime, toTime } = this.state;
         const alertTable = this.createAllAlertsTable();
         return (
             <div className="all-alerts-container">
@@ -311,7 +358,7 @@ export class AllAlerts extends React.Component<any, any> {
                                     Resource Group&nbsp;&nbsp;&nbsp;
                                 <i className="fa fa-info-circle"></i>
                                 </label>
-                                <select className="form-control" name="resourceGroup" value={state.resourceGroup} onChange={this.handleStateChange}>
+                                <select className="form-control" name="resourceGroup" value={resourceGroup} onChange={this.handleStateChange}>
                                     <option value="">Select Resource Group</option>
                                     {this.createSelectbox(this.resourceGroup)}
                                 </select>
@@ -323,9 +370,9 @@ export class AllAlerts extends React.Component<any, any> {
                                     Resources&nbsp;&nbsp;&nbsp;
                                 <i className="fa fa-info-circle"></i>
                                 </label>
-                                <select className="form-control" name="resource" value={state.resource} onChange={this.handleStateChange}>
+                                <select className="form-control" name="resource" value={resource} onChange={this.handleStateChange}>
                                     <option value="">Select Resources</option>
-                                    {this.createSelectbox(this.resources[state.resourceGroup])}
+                                    {this.createSelectbox(this.resources[resourceGroup])}
                                 </select>
                             </div>
                         </div>
@@ -336,9 +383,37 @@ export class AllAlerts extends React.Component<any, any> {
                                 <i className="fa fa-info-circle"></i>
                                 </label>
                                 <div>
-                                    <input className="timeRangeBox" onClick={this.onClickopenTimeRangePopup} type="text" />
+                                    <select className="form-control" value={currentTime} onClick={this.onClickopenTimeRangePopup} id="timeRange">
+                                    </select>
                                 </div>
                             </div>
+                            {openTimeRange && <div className="absolute-time-range-box">
+                                <div className="absolute-time-range-left">
+                                    <h3>Absolute time range</h3>
+                                    <form>
+                                        <div className="form-group">
+                                            <label htmlFor="From" className="d-block">From</label>
+                                            <input type="text" className="input-group-text" placeholder="now-3h" value={fromTime} />
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="To" className="d-block">To</label>
+                                            <input type="text" className="input-group-text" placeholder="now" value={toTime} />
+                                        </div>
+                                        <div className="form-group">
+                                            <button className="blue-button">Apply time range</button>
+                                        </div>
+                                    </form>
+                                    <div className="absolute-time-text">
+                                        It looks like you haven't used this timer picker before. As soon as you enter some time intervals, recently used intervals will appear here.
+                                        Read the documentation to find out more about how to enter custom time ranges.
+                                         </div>
+                                </div>
+                                <div className="absolute-time-range-right">
+                                    <select className="form-control" value={currentTime} onChange={this.setTimeValue} id="timeRange">
+                                        {this.displayTimeRange()}
+                                    </select>
+                                </div>
+                            </div>}
                         </div>
                         <div className="col-lg-2 col-md-3 col-sm-12">
                             <div className="form-group filter-control-group">
@@ -346,7 +421,7 @@ export class AllAlerts extends React.Component<any, any> {
                                     Monitor services&nbsp;&nbsp;&nbsp;
                                 <i className="fa fa-info-circle"></i>
                                 </label>
-                                <select className="form-control" name="monitorService" value={state.monitorService} onChange={this.handleStateChange}>
+                                <select className="form-control" name="monitorService" value={monitorService} onChange={this.handleStateChange}>
                                     <option value="">Select Monitor Services</option>
                                     {this.createSelectbox(this.monitoringServices)}
                                 </select>
@@ -358,7 +433,7 @@ export class AllAlerts extends React.Component<any, any> {
                                     Alert Type&nbsp;&nbsp;&nbsp;
                                 <i className="fa fa-info-circle"></i>
                                 </label>
-                                <select className="form-control" name="alertType" value={state.alertType} onChange={this.handleStateChange}>
+                                <select className="form-control" name="alertType" value={alertType} onChange={this.handleStateChange}>
                                     <option value="">Select Alert Type</option>
                                     {this.createSelectbox(this.alertTypes)}
                                 </select>
@@ -370,7 +445,7 @@ export class AllAlerts extends React.Component<any, any> {
                                     Serverity&nbsp;&nbsp;&nbsp;
                                 <i className="fa fa-info-circle"></i>
                                 </label>
-                                <select className="form-control" name="severity" value={state.severity} onChange={this.handleStateChange}>
+                                <select className="form-control" name="severity" value={severity} onChange={this.handleStateChange}>
                                     <option value="">Select Severity</option>
                                     {this.createSelectbox(this.severity)}
                                 </select>
@@ -382,7 +457,7 @@ export class AllAlerts extends React.Component<any, any> {
                                     Alert state&nbsp;&nbsp;&nbsp;
                                 <i className="fa fa-info-circle"></i>
                                 </label>
-                                <select className="form-control" name="alertState" value={state.alertState} onChange={this.handleStateChange}>
+                                <select className="form-control" name="alertState" value={alertState} onChange={this.handleStateChange}>
                                     <option value="Select Alert State">Select Alert State</option>
                                     {this.createSelectbox(this.alertStates)}
                                 </select>
@@ -477,7 +552,6 @@ export class AllAlerts extends React.Component<any, any> {
                         </PopoverBody>
                     </UncontrolledPopover>
                 }
-                <TimeRangePopup ref={this.timeRangeRef} />
             </div>
         );
     }
