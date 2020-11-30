f351184605f7da32591ef1d8077d9aee95b951d0
manojkrjoshi <manojkr.joshi@gmail.com>
2020-11-30 18:44:59 +0530

    graph data fixed from postgres database

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 const ALERT_URL = "alert.service.com";
 const SEARCH_URL = "search.service.com";
 
-const searchSrvUrl = `http://100.64.108.25:8092`;
-const alertSrvUrl = `http://100.64.108.25:5055`;
-const ticketSrvUrl = `http://100.64.108.25:7100/api`;
-// const searchSrvUrl = `http://localhost:8092`;
-// const alertSrvUrl = `http://localhost:5055`;
-// const ticketSrvUrl = `http://localhost:7100/api`;
+// const searchSrvUrl = `http://100.64.108.25:8092`;
+// const alertSrvUrl = `http://100.64.108.25:5055`;
+// const ticketSrvUrl = `http://100.64.108.25:7100/api`;
+const searchSrvUrl = `http://localhost:8092`;
+const alertSrvUrl = `http://localhost:5055`;
+const ticketSrvUrl = `http://localhost:7100/api`;
 
 export const config = {
   basePath: "/plugins/xformation-alertmanager-ui-plugin/page",
@@ -17,8 +17,10 @@ export const config = {
   TOP_ALERT_TODAY: `${alertSrvUrl}/api/topAlertToday`,
   GET_ALERT_VOLUME_DATA: `${alertSrvUrl}/api/getAlertVolumeData`,
   GET_ALERT_VOLUME_BY_STATUS: `${alertSrvUrl}/api/getAlertVolumeByStatus`,
-  GET_AVG_RESP_TIME_DATA: `${alertSrvUrl}/api/getAvgResponseTime`,
-  GET_AVG_WAIT_TIME_DATA: `${alertSrvUrl}/api/getWaitTimeGraphData`,
+  // GET_AVG_RESP_TIME_DATA: `${alertSrvUrl}/api/getAvgResponseTime`,
+  GET_AVG_RESP_TIME_DATA: `${alertSrvUrl}/api/getAvgResponseTimeGraphDataFromDb`,
+  // GET_AVG_WAIT_TIME_DATA: `${alertSrvUrl}/api/getWaitTimeGraphData`,
+  GET_AVG_WAIT_TIME_DATA: `${alertSrvUrl}/api/getWaitTimeGraphDataFromDb`,
   TOTAL_ALERTS: `${searchSrvUrl}/search/totalRecords`,
   GET_TICKETS_BY_GUID_URL: `${ticketSrvUrl}/alertTicketsByGuid`,
   GET_TEAM_MATRICS_DATA_URL: `${ticketSrvUrl}/getTeamMatricsData`,
